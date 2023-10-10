d4179251b3e027af8f82e40cd146a5b0a7a05776
Najwan <144207666+Najwan786@users.noreply.github.com>
2023-10-10 23:07:25 +0400

    practice5.py

--- Chapter 1- Getting Started with Python/Exercises/practice5.py
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -1,6 +1,8 @@
 #write four strings in different variables and print the output as one string (String concantenation)
 name= "Najwan"
-age= 19
-year= 2004
 course= "cybersecurity"
-print (name + age + year + course)
+nationality= "INDIAN"
+School= "SIS"
+
+Result = (name + "" +  course + "" +  ""  +nationality + "" + School)
+print(Result)
