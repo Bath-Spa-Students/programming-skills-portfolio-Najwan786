0abe52036e54080e9814790b6f10fe6a6680bfda
Najwan <144207666+Najwan786@users.noreply.github.com>
2023-11-03 19:04:54 +0400

    Chapter 5 Practice

---
+++ Chapter 5- Dictionaries/Exercises/Practice1.py
@@ -0,0 +1,29 @@
+# Tuples
+tup = (1,2,3)
+print(type(tup),tup)
+
+# TO CHECK LIST TYPR
+List =[1,2,3,4,5]
+print(type(List),List)
+
+# we cant change tuples
+
+#dictionaries
+# creation of dictionary
+msg= {}
+print(msg)
+
+#check the type of dictionary
+#to add something in dictionary
+example = {'color' : 'blue' ,
+           'fruit': 'mango','place' : 'Sharjah'}
+print(example)
+
+#lets check if we want to return one value , so its is a dictionary
+
+Example = {'NAME' : 'NAJWAN', 
+           'ROLL NO.' : '786',
+           'FATHERS NAME' : 'ABDUL NASER'}
+print(Example.get("NAME" , "NAJWAN"))
+
+
